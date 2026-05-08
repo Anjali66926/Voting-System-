7362525d9edca8998f5caed5c19e6473868c2d54
Anjali66926 <anjalianakale88@gmail.com>
2026-05-08 21:46:06 +0530

    Add files via upload

---
+++ TestConnection.java
@@ -0,0 +1,12 @@
+import java.sql.Connection;
+
+public class TestConnection {
+
+    public static void main(String[] args) {
+        try (Connection con = DBConnection.getConnection()) {
+            System.out.println("Connected Successfully!");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
